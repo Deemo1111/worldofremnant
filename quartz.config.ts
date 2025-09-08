7d82d9c458a130657c605623facf78866c960cf4
Yudha Kristianto <55428654+Deemo1111@users.noreply.github.com>
2025-09-08 18:57:33 +0300

    fix

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,6 +12,7 @@ const config: QuartzConfig = {
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
+    favicon: "./static/favicon.png",
     analytics: {
       provider: "plausible",
     },
